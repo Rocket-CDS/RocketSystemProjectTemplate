c9cf25599f0fdffc7d7529b3b045cef4f56be17d
leedavi <dcl@agence-sesame.fr>
2022-05-24 08:38:50 +0200

    upgrade code to new structure

--- API/StartConnect.cs
+++ API/StartConnect.cs
@@ -1,4 +1,6 @@
 ﻿using DNNrocketAPI.Components;
+using Rocket.AppThemes.Components;
+using RocketPortal.Components;
 using RocketSystemProjectTemplate.Components;
 using Simplisity;
 using System;
@@ -16,8 +18,17 @@ namespace RocketSystemProjectTemplate.API
         private Dictionary<string, string> _passSettings;
         private SessionParams _sessionParams;
         private UserParams _userParams;
+        private AppThemeLimpet _appTheme;
         private AppThemeSystemLimpet _appThemeSystem;
+        private AppThemeSystemLimpet _appThemeContent;
         private PortalContentLimpet _portalContent;
+        private PortalLimpet _portalData;
+        private RemoteModule _remoteModule;
+        private string _dataRef;
+        private string _moduleRef;
+        private string _org;
+        private Dictionary<string, object> _dataObjects;
+        private OrganisationLimpet _orgData;
 
         public override Dictionary<string, object> ProcessCommand(string paramCmd, SimplisityInfo systemInfo, SimplisityInfo interfaceInfo, SimplisityInfo postInfo, SimplisityInfo paramInfo, string langRequired = "")
         {
@@ -38,9 +49,6 @@ namespace RocketSystemProjectTemplate.API
                     break;
                 case "rocketsystem_delete":
                     strOut = RocketSystemDelete();
-                    break;
-                case "rocketsystem_valid":
-                    strOut = RocketSystemValid();
                     break;
                 case "rocketsystem_adminpanel":
                     strOut = AdminPanel();
@@ -78,140 +86,77 @@ namespace RocketSystemProjectTemplate.API
             return rtnDic;
 
         }
-
+        private string GetSystemTemplate(string templateName)
+        {
+            var razorTempl = _appTheme.GetTemplate(templateName);
+            if (razorTempl == "") razorTempl = _appThemeSystem.GetTemplate(templateName);
+            if (razorTempl == "") razorTempl = _appThemeContent.GetTemplate(templateName);
+            return razorTempl;
+        }
         private string RocketSystemSave()
         {
             var portalId = _paramInfo.GetXmlPropertyInt("genxml/hidden/portalid"); // we may have passed selection
             if (portalId >= 0)
             {
-                var portalContent = new PortalContentLimpet(portalId, _sessionParams.CultureCodeEdit);
-                if (portalContent.PortalId >= 0) portalContent.Save(_postInfo);
-                _portalContent = new PortalContentLimpet(portalId, _sessionParams.CultureCodeEdit); // reload portal data after save (for langauge change)
-                var razorTempl = _appThemeSystem.GetTemplate("RocketSystem.cshtml");
-                var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, null, _passSettings, _sessionParams, true);
-                if (pr.StatusCode != "00") return pr.ErrorMsg;
-                return pr.RenderedText;
+                _portalContent.Save(_postInfo);
+                _portalData.Record.SetXmlProperty("genxml/systems/" + _systemData.SystemKey + "setup", "True");
+                _portalData.Update();
+                return RocketSystem();
             }
             return "Invalid PortalId";
         }
         private String RocketSystem()
+        {
+            var razorTempl = GetSystemTemplate("RocketSystem.cshtml");
+            var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, _dataObjects, _passSettings, _sessionParams, true);
+            if (pr.StatusCode != "00") return pr.ErrorMsg;
+            return pr.RenderedText;
+        }
+        private String RocketSystemInit()
+        {
+            var newportalId = _paramInfo.GetXmlPropertyInt("genxml/hidden/newportalid");
+            if (newportalId > 0)
             {
-                try
-                {
-                    _portalContent = new PortalContentLimpet(_portalContent.PortalId, _sessionParams.CultureCodeEdit);
-                    var razorTempl = _appThemeSystem.GetTemplate("RocketSystem.cshtml");
-                    var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, null, _passSettings, _sessionParams, true);
-                    if (pr.StatusCode != "00") return pr.ErrorMsg;
-                    return pr.RenderedText;
-                }
-            catch (Exception ex)
-                {
-                    return ex.ToString();
-                }
+                _portalContent = new PortalContentLimpet(newportalId, _sessionParams.CultureCodeEdit);
+                _portalContent.Validate();
+                _portalContent.Update();
             }
-            private String RocketSystemInit()
+            return "";
+        }
+        private String RocketSystemDelete()
+        {
+            var portalId = _paramInfo.GetXmlPropertyInt("genxml/hidden/portalid");
+            if (portalId > 0)
             {
-                try
-                {
-                    var newportalId = _paramInfo.GetXmlPropertyInt("genxml/hidden/newportalid");
-                    if (newportalId > 0)
-                    {
-                        _portalContent = new PortalContentLimpet(newportalId, _sessionParams.CultureCodeEdit);
-                        _portalContent.Validate();
-                        _portalContent.Update();
-                    }
-                    return "";
-                }
-                catch (Exception ex)
-                {
-                    return ex.ToString();
-                }
+                _portalContent = new PortalContentLimpet(portalId, _sessionParams.CultureCodeEdit);
+                _portalContent.Delete();
             }
-            private String RocketSystemDelete()
-            {
-                try
-                {
-                    var portalId = _paramInfo.GetXmlPropertyInt("genxml/hidden/portalid");
-                    if (portalId > 0)
-                    {
-                        _portalContent = new PortalContentLimpet(portalId, _sessionParams.CultureCodeEdit);
-                        _portalContent.Delete();
-                    }
-                    return "";
-                }
-                catch (Exception ex)
-                {
-                    return ex.ToString();
-                }
-            }
-            private String RocketSystemValid()
-            {
-                try
-                {
-                    var portalId = _paramInfo.GetXmlPropertyInt("genxml/hidden/portalid");
-                    if (portalId > 0)
-                    {
-                        _portalContent = new PortalContentLimpet(portalId, _sessionParams.CultureCodeEdit);
-                        if (!_portalContent.Valid)
-                        {
-                            var razorTempl = _appThemeSystem.GetTemplate("InvalidSystem.cshtml");
-                            var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, null, _passSettings, _sessionParams, true);
-                            if (pr.StatusCode != "00") return pr.ErrorMsg;
-                            return pr.RenderedText;
-                        }
-                    }
-                    return "";
-                }
-                catch (Exception ex)
-                {
-                    return ex.ToString();
-                }
-            }
+            return "";
+        }
         private string AdminPanel()
         {
-            try
-            {
-                var razorTempl = _appThemeSystem.GetTemplate("AdminPanel.cshtml");
-                var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, null, _passSettings, _sessionParams, true);
-                if (pr.StatusCode != "00") return pr.ErrorMsg;
-                return pr.RenderedText;
-            }
-            catch (Exception ex)
-            {
-                return ex.ToString();
-            }
+            var razorTempl = GetSystemTemplate("AdminPanel.cshtml");
+            var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, _dataObjects, _passSettings, _sessionParams, true);
+            if (pr.StatusCode != "00") return pr.ErrorMsg;
+            return pr.RenderedText;
         }
         private string AdminPanelHeader()
         {
-            try
-            {
-                var razorTempl = _appThemeSystem.GetTemplate("AdminPanelHeader.cshtml");
-                var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, null, _passSettings, _sessionParams, true);
-                if (pr.StatusCode != "00") return pr.ErrorMsg;
-                return pr.RenderedText;
-            }
-            catch (Exception ex)
-            {
-                return ex.ToString();
-            }
+            var razorTempl = GetSystemTemplate("AdminPanelHeader.cshtml");
+            var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, _dataObjects, _passSettings, _sessionParams, true);
+            if (pr.StatusCode != "00") return pr.ErrorMsg;
+            return pr.RenderedText;
         }
         private string ReloadPage()
         {
-            try
-            {
-                // user does not have access, logoff
-                UserUtils.SignOut();
+            // user does not have access, logoff
+            UserUtils.SignOut();
 
-                var portalAppThemeSystem = new AppThemeDNNrocketLimpet("rocketportal");
-                var razorTempl = portalAppThemeSystem.GetTemplate("Reload.cshtml");
-                var pr = RenderRazorUtils.RazorProcessData(razorTempl, _portalContent, null, _passSettings, _sessionParams, true);
-                if (pr.StatusCode != "00") return pr.ErrorMsg;
-                return pr.RenderedText;
-            }
-            catch (Exception ex)
-            {
-                return ex.ToString();
-            }
+            var portalAppThemeSystem = new AppThemeDNNrocketLimpet("rocketportal");
+            var razorTempl = portalAppThemeSystem.GetTemplate("Reload.cshtml");
+            var pr = RenderRazorUtils.RazorProcessData(razorTempl, null, _dataObjects, _passSettings, _sessionParams, true);
+            if (pr.StatusCode != "00") return pr.ErrorMsg;
+            return pr.RenderedText;
         }
         public string InitCmd(string paramCmd, SimplisityInfo systemInfo, SimplisityInfo interfaceInfo, SimplisityInfo postInfo, SimplisityInfo paramInfo, string langRequired = "")
         {
@@ -219,10 +164,14 @@ namespace RocketSystemProjectTemplate.API
             _paramInfo = paramInfo;
             _systemData = new SystemLimpet(systemInfo.GetXmlProperty("genxml/systemkey"));
             _appThemeSystem = new AppThemeSystemLimpet(_systemData.SystemKey);
+            _appThemeContent = new AppThemeSystemLimpet("rocketcontent");
             _rocketInterface = new RocketInterface(interfaceInfo);
             _sessionParams = new SessionParams(_paramInfo);
             _userParams = new UserParams(_sessionParams.BrowserSessionId);
             _passSettings = new Dictionary<string, string>();
+            _orgData = new OrganisationLimpet();
+            _moduleRef = _paramInfo.GetXmlProperty("genxml/hidden/moduleref");
+            if (_moduleRef == "") _moduleRef = _paramInfo.GetXmlProperty("genxml/remote/moduleref");
 
             // Assign Langauge
             DNNrocketUtils.SetCurrentCulture();
@@ -233,18 +182,51 @@ namespace RocketSystemProjectTemplate.API
 
             var portalid = _paramInfo.GetXmlPropertyInt("genxml/hidden/portalid");
             if (portalid >= 0 && PortalUtils.GetPortalId() == 0)
+            {
+                _remoteModule = new RemoteModule(portalid, _moduleRef);
                 _portalContent = new PortalContentLimpet(portalid, _sessionParams.CultureCodeEdit); // Portal 0 is admin, editing portal setup
+            }
             else
             {
+                _remoteModule = new RemoteModule(PortalUtils.GetPortalId(), _moduleRef);
                 _portalContent = new PortalContentLimpet(PortalUtils.GetPortalId(), _sessionParams.CultureCodeEdit);
                 if (!_portalContent.Active) return "";
             }
-            // [TODO]: Users should only have access to their own services for setup on portal 0.
-            // [TODO]: Private admin needs to allow access for managers.
-            // [TODO]: Public facing API should allow access for all users.
+            _portalData = new PortalLimpet(_portalContent.PortalId);
+            _dataRef = _remoteModule.DataRef;
+
+            if (_dataRef == "")
+            {
+                // If we are editing from the AdminPanel, we will not have a moduleRef, only a dataref.
+                _dataRef = _paramInfo.GetXmlProperty("genxml/hidden/dataref");
+                if (_dataRef == "") _dataRef = _paramInfo.GetXmlProperty("genxml/remote/dataref");
+            }
+            _org = _remoteModule.Organisation;
+            if (_org == "") _org = _orgData.DefaultOrg();
+
+            _appTheme = new AppThemeLimpet(_portalContent.PortalId, _remoteModule.AppThemeViewFolder, _remoteModule.AppThemeViewVersion, _org);
 
             var securityData = new SecurityLimpet(_portalContent.PortalId, _systemData.SystemKey, _rocketInterface, -1, -1);
-            paramCmd = securityData.HasSecurityAccess(paramCmd, "rocketsystem_login");
+
+            _dataObjects = new Dictionary<string, object>();
+            _dataObjects.Add("remotemodule", _remoteModule);
+            _dataObjects.Add("apptheme", _appTheme);
+            _dataObjects.Add("appthemesystem", _appThemeSystem);
+            _dataObjects.Add("appthemecontent", _appThemeContent);
+            _dataObjects.Add("portalcontent", _portalContent);
+            _dataObjects.Add("portaldata", _portalData);
+            _dataObjects.Add("securitydata", securityData);
+            _dataObjects.Add("systemdata", _systemData);
+
+            if (paramCmd.StartsWith("remote_"))
+            {
+                var sk = _paramInfo.GetXmlProperty("genxml/remote/securitykeyedit");
+                if (!UserUtils.IsEditor() && _portalData.SecurityKeyEdit != sk) paramCmd = "";
+            }
+            else
+            {
+                paramCmd = securityData.HasSecurityAccess(paramCmd, "rocketsystem_login");
+            }
 
             return paramCmd;
         }
